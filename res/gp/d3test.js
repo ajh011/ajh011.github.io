c5b2f79a69ef080820848660c707e489988a552b
ajh011 <46264051+ajh011@users.noreply.github.com>
2022-04-27 17:37:24 -0400

    Update d3test.js

--- res/gp/d3test.js
+++ res/gp/d3test.js
@@ -1,25 +1,6 @@
-import functionPlot from "function-plot";
-
-let contentsBounds = document.body.getBoundingClientRect();
-let width = 800;
-let height = 500;
-let ratio = contentsBounds.width / width;
-width *= ratio;
-height *= ratio;
-
 functionPlot({
-    target: "#function1",
-    width,
-    height,
-    yAxis: { domain: [-1, 9] },
-    grid: true,
-    data: [
-      {
-        fn: "x^2",
-        derivative: {
-          fn: "2 * x",
-          updateOnMouseMove: true
-        }
-      }
-    ]
-  });
+    target: '#quadratic',
+    data: [{
+      fn: 'x^2'
+    }]
+  })
